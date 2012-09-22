ae79670fa364d01d5d81017225405a9ba47e0713
Levi <lnunnink@riskalyze.com>
2012-09-21 22:28:54 -0700

    Initial commit

---
+++ TumbleKit/TumbleKit.h
@@ -0,0 +1,13 @@
+//
+//  TumbleKit.h
+//  TumbleKit
+//
+//  Created by Levi Nunnink on 9/21/12.
+//  Copyright (c) 2012 Culturezoo LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TumbleKit : NSObject
+
+@end
---
+++ TumbleKit/TumbleKit.m
@@ -0,0 +1,13 @@
+//
+//  TumbleKit.m
+//  TumbleKit
+//
+//  Created by Levi Nunnink on 9/21/12.
+//  Copyright (c) 2012 Culturezoo LLC. All rights reserved.
+//
+
+#import "TumbleKit.h"
+
+@implementation TumbleKit
+
+@end
